0f8b23aac9a8d59e746209998320aba95418fa1d
kyusuf97 <47285417+kyusuf97@users.noreply.github.com>
2020-01-29 13:21:51 -0800

    Update code for demo1 with angle input functionality

---
+++ Code/Good_Motor/Good_Motor.ino
@@ -0,0 +1,199 @@
+
+#define CCW 0
+#define CW 1
+
+int motorPin1 = 11;
+int motorPin2 = 10;
+int motorPin3 = 9;
+int motorPin4 = 8;
+
+
+
+int dir;        /* Ideally must be set to either CCW or CW */
+int motor_state = 0;
+
+int opticalPin = 2;
+int Read = 0;
+int lastRead;
+double last_angle = 0;
+double angle_counter = 0;
+double angle_input = 0;
+int opticalWindows = 50;
+
+void setup() {
+
+  noInterrupts(); //disable all interrupts
+
+// initialize timer1
+
+  TCCR1A = 0;
+  TCCR1B = 0;
+  
+  TCNT1 = 56000; //preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);  //256 prescaler
+  TIMSK1 |= (1 << TOIE1);   //enable timer overflow interrupt
+  interrupts(); //enable all interrupts
+  
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  
+  pinMode(opticalPin, INPUT);
+
+  lastRead = digitalRead(opticalPin);
+
+  Serial.begin(9600);
+}
+
+ISR(TIMER1_OVF_vect) //interrupts service routine
+{
+  TCNT1 = 56000;
+  if (motor_state == 0)
+    motor_state = 1;
+  else if (motor_state == 1)
+    motor_state = 2;
+  else if (motor_state == 2)
+    motor_state = 3;
+  else 
+    motor_state = 0;
+}
+    
+   
+
+
+void loop() {
+  
+  if (Serial.available() > 0) {
+    angle_input = Serial.parseInt();   
+    Serial.println(angle_input);    
+  }
+  
+
+  
+  if (last_angle != angle_counter)
+  {
+    Serial.println(angle_counter);
+    last_angle = angle_counter;
+  }
+
+ 
+  
+  Read = digitalRead(opticalPin);
+  if (lastRead != Read) 
+  {
+    if(dir == CCW)
+    {
+      angle_counter = angle_counter + 3.6;
+      if (angle_counter >= 360)
+        angle_counter = angle_counter - 360;
+      lastRead = Read;
+    }
+    else
+    {
+      angle_counter = angle_counter - 3.6;
+      if (angle_counter < 0)
+        angle_counter = angle_counter + 360;
+      lastRead = Read;
+    } 
+  }
+
+  if((angle_input - angle_counter) >= 4)
+  {
+      dir = CCW;
+
+      
+      if (motor_state == 0) 
+      {
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, HIGH);
+      }
+      else if (motor_state == 1) 
+      {
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, HIGH);
+      }
+       
+      else if (motor_state == 2) 
+      {
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
+      }
+      else if (motor_state == 3)
+      {
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
+      }
+    else 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, LOW);
+    } 
+    
+   }
+
+   
+  else if ((angle_input - angle_counter) <= -4)
+  {
+
+    dir = CW;
+
+    if (motor_state == 0) 
+    {
+      digitalWrite(motorPin1, HIGH);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, HIGH);
+    }
+    else if (motor_state == 1) 
+    {
+      digitalWrite(motorPin1, HIGH);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, HIGH);
+      digitalWrite(motorPin4, LOW);
+    }
+     
+    else if (motor_state == 2) 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, HIGH);
+      digitalWrite(motorPin3, HIGH);
+      digitalWrite(motorPin4, LOW);
+    }
+    else if (motor_state == 3)
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, HIGH);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, HIGH);
+    }  
+    else 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, LOW);
+    } 
+  
+  }
+
+  else 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin2, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin4, LOW);
+    }
+  
+
+}
--- Code/StepperCode/StepperCode.ino
+++ Code/StepperCode/StepperCode.ino
@@ -10,67 +10,107 @@ int motorPin5 = 4;
 int motorPin6 = 5;
 int delayTime = 100;  /* In milliseconds, note that 100ms is about max for demo1 motor */ 
 int dir = CCW;        /* Ideally must be set to either CCW or CW */
+int motor_state = 0;
 
 void setup() {
+
+  noInterrupts(); //disable all interrupts
+
+// initialize timer1
+
+  TCCR1A = 0;
+  TCCR1B = 0;
+  
+  TCNT1 = 34286; //preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);  //256 prescaler
+  TIMSK1 |= (1 << TOIE1);   //enable timer overflow interrupt
+  interrupts(); //enable all interrupts
+  
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
   pinMode(motorPin5, OUTPUT);
   pinMode(motorPin6, OUTPUT);
+
+  // motor pins 2,4,6 are always low
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin6, LOW);
 }
+
+ISR(TIMER1_OVF_vect) //interrupts service routine
+{
+  TCNT1 = 62411;
+  if (motor_state == 0)
+    motor_state = 1;
+  else if (motor_state == 1)
+    motor_state = 2;
+  else 
+    motor_state = 0;
+}
+    
+   
+
 
 void loop() {
 
-  if( dir == CCW ){
-    digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, LOW);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
-  
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, HIGH);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
-    
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, HIGH);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, LOW);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
-  }
-    
-  else{
-    digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, LOW);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
-  
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, HIGH);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, LOW);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
-    
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    digitalWrite(motorPin5, HIGH);
-    digitalWrite(motorPin6, LOW);
-    delay(delayTime);
+
+
+  if(dir == CCW)
+  {
+      if (motor_state == 0) 
+      {
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin5, LOW);
+      }
+      else if (motor_state == 1) 
+      {
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin5, LOW);
+      }
+       
+      else if (motor_state == 2) 
+      {
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin5, HIGH);
+      }
+      else 
+      {
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin5, LOW);
+      }
+   }
+  else 
+  {
+    if (motor_state == 0) 
+    {
+      digitalWrite(motorPin1, HIGH);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin5, LOW);
+    }
+    else if (motor_state == 1) 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin5, HIGH);
+    }
+     
+    else if (motor_state == 2) 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin3, HIGH);
+      digitalWrite(motorPin5, LOW);
+    }
+    else 
+    {
+      digitalWrite(motorPin1, LOW);
+      digitalWrite(motorPin3, LOW);
+      digitalWrite(motorPin5, LOW);
+    }  
   }
 }
